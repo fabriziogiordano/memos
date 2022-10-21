1fa1cce17df0c0c34467836b43a9c86b5426edc6
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2022-10-21 19:28:05 -0400

    add firebase config for analytics

--- src/frontend/assets/js/firebase.js
+++ src/frontend/assets/js/firebase.js
@@ -2,7 +2,11 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics, logEvent } from "firebase/analytics";
 
-const firebaseConfig = { measurementId: "G-39XDR0NHSS" };
+const firebaseConfig = {
+  appId: "1:70471515696:web:5fcbf693f473ff4b4e77f5",
+  projectId: "streeteasyclone",
+  measurementId: "G-39XDR0NHSS",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
